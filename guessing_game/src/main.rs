4e942f3bcf3762ed559a0e3ecf1ce44868477ccc
fheutz <f.heutzenroeder@gmail.com>
2021-07-18 13:12:21 +0200

    chapter 2 Guessing game added

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
-
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the Number!");
 
+    let secret = rand::thread_rng().gen_range(1..101);
+    loop {
     println!("Please input your Guess");
     let mut guess = String::new();
 
@@ -11,5 +14,29 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     
-    println!("Your guessed {}", guess)
+    // You can fail the program on ERR while parsing with .expect() method
+    // let guess : u32 = guess.trim().parse().expect("Please type a number!");
+
+    // Its better to handle the error with match and using Ok and Err as arms
+    let guess : u32 = match guess.trim().parse(){
+        Ok(num) => num,
+        Err(_) => {
+            println!("[ERR] invalid input please type a number");
+            continue;
+        }
+    };
+    
+
+
+    println!("Your guessed {}", guess);
+    match guess.cmp(&secret){
+        Ordering::Less => print!("\tbut its too smol"),
+        Ordering::Greater => print!("\tbut its too big"),
+        Ordering::Equal => {
+            print!("\tThat is right you win!\n");
+            break;
+        }
+    }
+
+    }
 }
